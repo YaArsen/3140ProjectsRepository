eb3446fab4f2d4623fc065dc67f43556b8cff083
YaArsen <arsen.yakubyants69@bcmail.cuny.edu>
2025-08-09 16:33:10 -0400

    Create recipe.js

---
+++ digital-cookbook/models/recipe.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    ingredients: {
+        type: [String],
+        required: true
+    },
+    instructions: {
+        type: String,
+        required: true
+    },
+    prepTimeInMinutes: {
+        type: Number,
+        min: [1, 'Prep time must be a positive number']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+module.exports = Recipe;
